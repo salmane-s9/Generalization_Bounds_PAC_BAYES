6d006e2df71e6d3550882454175d9f7fbeb0f1af
salmane-s9 <41329788+salmane-s9@users.noreply.github.com>
2019-02-15 14:17:53 +0100

    Add files via upload

---
+++ PacBayes_Loss.py
@@ -0,0 +1,44 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import torch
+import torch.nn as nn
+from torch.nn.utils import parameters_to_vector
+from utils import calc_BRE_term,calc_kullback_leibler
+
+
+
+# In[ ]:
+
+
+class PacBayesLoss(nn.Module):
+    
+    def __init__(self, lambda_prior_ ,sigma_posterior_ , params, conf_param , Precision , 
+                 bound , data_size ,reduce=None, reduction='mean'):
+        
+        super(PacBayesLoss, self).__init__()
+        self.lambda_prior_ = nn.Parameter(lambda_prior_)
+        self.sigma_posterior_ = nn.Parameter(sigma_posterior_)
+        self.params = params.parameters()
+        self.flat_params = nn.Parameter(parameters_to_vector(params.parameters()))
+        self.precision = Precision
+        self.conf_param = conf_param
+        self.bound = bound
+        self.data_size = data_size
+        self.params_0 = torch.randn(self.flat_params.size())
+        self.d_size = sum(p.numel() for p in self.params if p.requires_grad)
+        
+        
+    def forward(self,parameters):
+        self.params = parameters
+        self.flat_params = nn.Parameter(parameters_to_vector(parameters))
+        Bre_loss = calc_BRE_term(self.precision, self.conf_param, self.bound, self.flat_params, 
+                                 self.params_0, self.lambda_prior_, self.sigma_posterior_, 
+                                 self.data_size, self.d_size)
+        
+        
+        return Bre_loss
+
---
+++ utils.py
@@ -0,0 +1,141 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import torch
+import torch.nn as nn
+from math import log,pi
+import numpy as np
+
+
+# In[2]:
+
+
+def calc_kullback_leibler(lambda_prior_ ,sigma_posterior_ ,params , params_0 , d_size):
+    # explicit calculation of KL divergence between prior N(0,lambda * Id) and posterior N(w, s)
+
+    assert (torch.is_tensor(lambda_prior_) and torch.is_tensor(sigma_posterior_) )
+    lambda_prior = torch.exp(2 * lambda_prior_ )
+    sigma_post = torch.exp(2 * sigma_posterior_ )
+
+    tr = torch.sum(sigma_post) / lambda_prior
+    
+    l2 = torch.norm(params -params_0)/ lambda_prior
+    d = d_size 
+
+    logdet_prior = d * torch.log(lambda_prior)
+#     sgndetA, logdet_post = torch.slogdet(torch.diag(sigma_post))
+    logdet_post = torch.log(torch.prod(sigma_post,dtype=torch.double)).float()
+#     assert sgndetA == 1.0
+
+    kl = (tr + l2 - d + logdet_prior - logdet_post ) / 2.
+
+    return kl
+
+def calc_BRE_term(Precision ,conf_param ,bound ,params , params_0,lambda_prior_ ,sigma_posterior_,data_size,d_size): 
+#   Explicit Calculation of the second term of the bound (BRE)
+
+
+    kl = calc_kullback_leibler(lambda_prior_, sigma_posterior_ ,params , params_0 , d_size)
+    
+    lambda_prior = torch.exp(2 * lambda_prior_ )
+    
+    assert bound > lambda_prior
+    log_log = 2* torch.log(Precision* torch.log(bound /lambda_prior))
+
+    m = data_size
+    log_ = log((((pi**2) * m)/(6* conf_param)))
+
+    bre = torch.sqrt((kl + log_log + log_ ) / (2 *(m-1)))
+
+    return bre
+
+def network_params(model):
+#       return network parameters and layers 
+    layers = []
+    ind = 0
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            shape = model.state_dict()[name].shape
+            params = np.ravel(param.data.numpy())
+            ind2 = np.size(params)
+            ind = ind2
+            layers.append((name,ind,shape))
+        
+    return layers
+
+
+# In[3]:
+
+
+class FeedForwardNeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+
+        self.fc1 = nn.Linear(input_size, hidden_size, bias =True)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes, bias =True)
+        
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+    
+    
+class FeedForwardNeuralNet2(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+
+        self.fc1 = nn.Linear(input_size, hidden_size, bias =True)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, hidden_size, bias =True)
+        self.fc3 = nn.Linear(hidden_size, num_classes, bias =True)
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        return out
+
+    
+class FeedForwardNeuralNet3(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+
+        self.fc1 = nn.Linear(input_size, hidden_size, bias =True)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, hidden_size, bias =True)
+        self.fc3 = nn.Linear(hidden_size, hidden_size, bias =True)
+        self.fc4 = nn.Linear(hidden_size, num_classes, bias =True)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        out = self.relu(out)
+        out = self.fc4(out)
+        return out
+
+
+# Another solution
+class FeedForwardNeuralNet3R(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+        self.model = nn.Sequential(torch.nn.Linear(input_size, hidden_size, bias =True),
+                                    torch.nn.ReLU(),
+                                    torch.nn.Linear(hidden_size, hidden_size, bias =True),
+                                    torch.nn.ReLU(),
+                                    torch.nn.Linear(hidden_size, hidden_size, bias =True),
+                                    torch.nn.ReLU(),
+                                    torch.nn.Linear(hidden_size, num_classes, bias =True),
+                                    )
+    def forward(self, x):
+        out = self.main(x)
+        return out
+
