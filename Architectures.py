efceef9ad10d86442fd659caacafb7ce0756e5eb
salmane-s9 <41329788+salmane-s9@users.noreply.github.com>
2019-03-08 12:13:48 +0100

    Add files via upload

---
+++ Architectures.py
@@ -0,0 +1,80 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import torch.nn as nn
+
+
+# In[2]:
+
+
+class FeedForwardNeuralNet(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+
+        self.fc1 = nn.Linear(input_size, hidden_size, bias =True)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes, bias =True)
+        
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
+    
+    
+class FeedForwardNeuralNet2(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+
+        self.fc1 = nn.Linear(input_size, hidden_size, bias =True)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, hidden_size, bias =True)
+        self.fc3 = nn.Linear(hidden_size, num_classes, bias =True)
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        return out
+
+    
+class FeedForwardNeuralNet3(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+
+        self.fc1 = nn.Linear(input_size, hidden_size, bias =True)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, hidden_size, bias =True)
+        self.fc3 = nn.Linear(hidden_size, hidden_size, bias =True)
+        self.fc4 = nn.Linear(hidden_size, num_classes, bias =True)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+        out = self.relu(out)
+        out = self.fc4(out)
+        return out
+
+
+class FeedForwardNeuralNet3R(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+        self.model = nn.Sequential(torch.nn.Linear(input_size, hidden_size, bias =True),
+                                    torch.nn.ReLU(),
+                                    torch.nn.Linear(hidden_size, hidden_size, bias =True),
+                                    torch.nn.ReLU(),
+                                    torch.nn.Linear(hidden_size, hidden_size, bias =True),
+                                    torch.nn.ReLU(),
+                                    torch.nn.Linear(hidden_size, num_classes, bias =True),
+                                    )
+    def forward(self, x):
+        out = self.main(x)
+        return out
+
