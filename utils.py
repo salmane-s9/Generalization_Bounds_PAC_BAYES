ade05196af369c3dbe4868bf3463024c414c3398
salmane-s9 <41329788+salmane-s9@users.noreply.github.com>
2019-03-11 09:13:22 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,13 +5,15 @@ import numpy as np
 
 
 
-def calc_kullback_leibler(lambda_prior, sigma_post, params, params_0, d_size):
-    # explicit calculation of KL divergence between prior N(0,lambda * Id) and posterior N(w, s)
-
-#     assert (torch.is_tensor(lambda_prior) and torch.is_tensor(sigma_post) )
+def calc_kullback_leibler(lambda_prior ,sigma_post ,params , params_0 , d_size):
+    
+    """
+    explicit calculation of KL divergence between prior N(0,lambda_prior * Id) and posterior N(flat_params, sigma_posterior_)
+    """
+    
     tr = torch.norm(sigma_post, p=1)/ lambda_prior
     
-    l2 = torch.pow(torch.norm(params -params_0, p=2),2)/ lambda_prior
+    l2 = torch.pow(torch.norm(params -params_0, p=2), 2)/ lambda_prior
     d = d_size 
 
     logdet_prior = d * torch.log(lambda_prior)
@@ -22,15 +24,17 @@ def calc_kullback_leibler(lambda_prior, sigma_post, params, params_0, d_size):
 
     return kl
 
-def calc_BRE_term(Precision, conf_param, bound, params, params_0, lambda_prior_, sigma_posterior_, data_size, d_size): 
-#   Explicit Calculation of the second term of the bound (BRE)
-
-    lambda_prior = torch.clamp(torch.exp(2 * lambda_prior_ ), min = 0, max = bound)
-    sigma_post = torch.exp(2 * sigma_posterior_ )
+def calc_BRE_term(Precision ,conf_param ,bound ,params , params_0,lambda_prior_ ,sigma_posterior_,data_size,d_size): 
+    """
+   Explicit Calculation of the second term of the optimization problem (BRE)
+    """
+ 
+    lambda_prior = torch.clamp(torch.exp(2 * lambda_prior_ ), min = 1e-38, max = bound - 1e-8)
+    sigma_post = torch.exp(2 * sigma_posterior_)
     
     kl = calc_kullback_leibler(lambda_prior, sigma_post ,params , params_0 , d_size)
-   
-    log_log = 2* torch.log(Precision* torch.log(bound /lambda_prior))
+    
+    log_log = 2* torch.log(Precision* (torch.log(bound /lambda_prior)))
 
     m = data_size
     log_ = log((((pi**2) * m)/(6* conf_param)))
@@ -40,7 +44,10 @@ def calc_BRE_term(Precision, conf_param, bound, params, params_0, lambda_prior_,
     return bre
 
 def network_params(model):
-#       return network parameters and layers 
+    """
+    Return a list containing names and shapes of neural network layers
+    """
+
     layers = []
     ind = 0
     for name, param in model.named_parameters():
@@ -52,8 +59,11 @@ def network_params(model):
             layers.append((name,ind,shape))
         
     return layers
-
-def load_train_weights(model, weights):
+    
+def load_train_weights(model ,weights):
+    """
+    Load trained weights into a neural network model
+    """
     pretrained_dict = torch.load(weights)
     model_dict = model.state_dict()
 
@@ -62,8 +72,10 @@ def load_train_weights(model, weights):
     model.state_dict()
     return model
 
-
-def test_error(*, loader, nn_model, device):
+def test_error(loader, nn_model, device):
+    """
+    Compute the empirical error of neural network on a dataset loader
+    """
     with torch.no_grad():
         correct = 0
         total = 0
@@ -77,5 +89,44 @@ def test_error(*, loader, nn_model, device):
 
             total += labels.size(0)
             correct += (predicted == labels.long()).sum().item()
+        error = 1. - correct / total
+        return(error)
+    
+def apply_weights(model, modified_parameters):
+    """
+    Modify the parameters of a neural network 
+    """
+    indi = 0
+    for name,ind,shape_ in network_params(model):
+        model.state_dict()[name].data.copy_(modified_parameters[indi:indi+ind].view(shape_)) 
+        indi += ind
+    return(model)
 
-        print('{0:.3f} '.format(1. - correct / total))
+
+def KL(Q, P):
+    """
+    Compute Kullback-Leibler (KL) divergence between distributions Q and P.
+    """
+    return sum([ q*log(q/p) if q > 0. else 0. for q,p in zip(Q,P) ])
+
+
+def KL_binomial(q, p):
+    """
+    Compute the KL-divergence between two Bernoulli distributions of probability
+    of success q and p. That is, Q=(q,1-q), P=(p,1-p).
+    """
+    return KL([q, 1.-q], [p, 1.-p])
+
+def solve_kl_sup(q, right_hand_side):
+    """
+    find x such that:
+        kl( q || x ) = right_hand_side
+        x > q
+    """
+    f = lambda x: KL_binomial(q, x) - right_hand_side
+
+    if f(1.0-1e-9) <= 0.0:
+        return 1.0-1e-9
+    else:
+        return optimize.brentq(f, q, 1.0-1e-9)
+     
